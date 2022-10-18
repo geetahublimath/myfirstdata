b982901c684c04776ec0b55d0af54e025cd3103d
hublimathgeeta <hublimathgeeta123@gmail.com>
2022-10-18 20:42:30 +0530

    code pushed

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,8 +13,77 @@ router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
+//Get-API Problem 1
+router.get('/movies', function(req,res){
+     console.log("The path params in the request are : ", req.params)
+     const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies)
+})
 
+////Get-API Problem 2
 
+router.get('/movies/:indexNumber',function(req,res){
+    const index=req.params.indexNumber
+    const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies[index])
+})
+//Get-API Problem 3
+router.get('/movies/:indexNumber',function(req,res){
+    const index=req.params.indexNumber
+    const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    if(index>(movies.length-1)&&Number(index)>=0){
+        res.send ("Invalid index")
+    }
+    else{
+        res.send(movies[index])
+    }
+    
+})
+//Get-API Problem 4
+const movie=[ {
+    id: 1,
+    name: 'The Shining'
+   }, {
+    id: 2,
+    name: 'Incendies'
+   }, {
+    id: 3,
+    name: 'Rang de Basanti'
+   }, {
+    id: 4,
+    name: 'Finding Nemo'
+   }]
+   router.get('/films', function(req,res){
+
+    res.send(movie)
+   })
+
+//Get-API Problem 5
+const movies=[ {
+    id: 1,
+    name: 'The Shining'
+   }, {
+    id: 2,
+    name: 'Incendies'
+   }, {
+    id: 3,
+    name: 'Rang de Basanti'
+   }, {
+    id: 4,
+    name: 'Finding Nemo'
+   }]
+   router.get('/film/:filmId',function(req,res){
+   const id=req.params.filmId
+   const moviearr=movies.find(film=>film.id=id)
+   if(moviearr)
+   {
+    res.send(moviearr)
+   }
+
+   else{
+    res.send("No movie exist with this id")
+   }
+   })
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
     // ':' denotes that the following part of route is a variable
