6308ac2361bf048d79c00b886ac81e3bfeb2fd7c
hublimathgeeta <hublimathgeeta123@gmail.com>
2022-10-21 18:47:41 +0530

    branch pushed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://hublimathgeeta:swami123@geeta.ygv2q9j.mongodb.net/test"
 , {
    useNewUrlParser: true 
 }
