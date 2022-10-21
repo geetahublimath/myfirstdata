e89aae6da20b74553291385874366b3c42e4d05b
hublimathgeeta <hublimathgeeta123@gmail.com>
2022-10-21 11:31:18 +0530

    Code pushed

--- src/routes/route.js
+++ src/routes/route.js
@@ -159,5 +159,50 @@ router.post( "/post-query-2", function (req, res){
     res.send( {data: finalArr , status: true})
 })
 
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ router.post('/persons', function (req, res) {
+    let votingAge=req.query.votingAge
+    let finalarrobj=[]
+    for(i=0;i<persons.length;i++){
+        if(persons[i].age>votingAge)
+        {
+            persons[i].votingStatus=true
+            
+            finalarrobj.push(persons[i])
+        }
+    
+       
+    }
+
+    res.send(  { Updatedpersons:finalarrobj }  )
+})
+
+
 
 module.exports = router;
